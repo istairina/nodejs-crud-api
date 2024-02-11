478633f4110fa1d19833717f98a08543efd6cdb4
Irina Migunova <ista.irina@gmail.com>
2024-02-12 02:36:55 +0300

    feat: add third test scenario

--- tests/users.test.ts
+++ tests/users.test.ts
@@ -1,10 +1,24 @@
 import { server } from "../src";
 import { describe, expect } from "@jest/globals";
 import { UsersType } from "../src/models/users";
+import { randomUUID } from "node:crypto";
 
 describe("Users endpoints", () => {
   const testPort = 5005;
   const testServerAddress = `http://localhost:${testPort}/api/users`;
+
+  let userId = "";
+
+  const data = {
+    username: "IM",
+    age: 88,
+    hobbies: ["chess", "reading"],
+  };
+
+  const mistakenData = {
+    username: "IM",
+    hobbies: ["chess", "reading"],
+  };
 
   beforeAll(() => {
     server.close();
@@ -20,21 +34,16 @@ describe("Users endpoints", () => {
   });
 
   test("it should create a user", async () => {
-    const data = {
-      username: "IM",
-      age: 88,
-      hobbies: ["chess", "reading"],
-    };
-
-    const mistakenData = {
-      username: "IM",
-      hobbies: ["chess", "reading"],
-    };
-
     const response = await fetch(testServerAddress, {
       method: "POST",
       body: JSON.stringify(data),
     });
+
+    const responseData = await response.text();
+    const regex =
+      /[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/;
+    const matchResult = responseData.match(regex);
+    userId = matchResult ? matchResult[0] : "";
 
     expect(response.status).toBe(201);
 
@@ -54,5 +63,30 @@ describe("Users endpoints", () => {
     const responseData = (await response.json()) as { results: UsersType[] };
 
     expect(responseData.results.length).toBeGreaterThan(0);
+  });
+
+  test("it should get a user by id", async () => {
+    const response = await fetch(`${testServerAddress}/${userId}`, {
+      method: "GET",
+    });
+
+    const responseData = await response.json();
+
+    expect(response.status).toBe(200);
+    expect(responseData.result).toEqual({ ...data, id: userId });
+
+    const responseWithFakeId = await fetch(`${testServerAddress}/fake-id`, {
+      method: "GET",
+    });
+
+    expect(responseWithFakeId.status).toBe(400);
+
+    const newId = randomUUID();
+
+    const responseWithNewId = await fetch(`${testServerAddress}/${newId}`, {
+      method: "GET",
+    });
+
+    expect(responseWithNewId.status).toBe(404);
   });
 });
